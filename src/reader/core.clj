76a08f93ca30260aec1a6a1b47bfabf7b0330da8
Akshatha Herle <akshatha@nilenso.com>
2020-10-13 21:54:29 +0530

    Move server functions to server namespace

--- src/reader/core.clj
+++ src/reader/core.clj
@@ -1,33 +1,7 @@
 (ns reader.core
   (:gen-class)
-  (:require [org.httpkit.server :as h]
-            [reader.config :as config]))
-
-(defonce server (atom nil))
-
-(defn handler
-  [request]
-  {:status 200
-   :body "mic check"})
-
-(defn start-app!
-  []
-  (reset! server (h/run-server handler {:port (config/port)}))
-  (println "Server started on port" (config/port)))
-
-(defn stop-app!
-  []
-  (when-not (nil? @server)
-    (@server :timeout 100)
-    (reset! server nil))
-  (println "Server stopped."))
-
-(defn restart-app!
-  []
-  (stop-app!)
-  (start-app!)
-  (println "Server restarted on port" (config/port)))
+  (:require [reader.server :as server]))
 
 (defn -main
-  [& args]
-  (start-app!))
+  [_]
+  (server/start-app!))
---
+++ src/reader/server.clj
@@ -0,0 +1,29 @@
+(ns reader.server
+  (:require [org.httpkit.server :as http]
+            [reader.config :as config]))
+
+(defonce server (atom nil))
+
+(defn handler
+  [_]
+  {:status 200
+   :body "mic check"})
+
+(defn start-app!
+  []
+  (reset! server (http/run-server handler {:port (config/port)}))
+  (println "Server started on port" (config/port)))
+
+(defn stop-app!
+  []
+  (when-not (nil? @server)
+    (@server :timeout 100)
+    (reset! server nil))
+  (println "Server stopped."))
+
+(defn restart-app!
+  []
+  (stop-app!)
+  (start-app!)
+  (println "Server restarted on port" (config/port)))
+
