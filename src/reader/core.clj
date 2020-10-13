0aa9919f75417275748fc8a1599c2cfbc2c7193b
Akshatha Herle <akshatha@nilenso.com>
2020-10-13 21:48:51 +0530

    Set up simple http server and add server functions

--- src/reader/core.clj
+++ src/reader/core.clj
@@ -1,6 +1,33 @@
 (ns reader.core
-  (:gen-class))
+  (:gen-class)
+  (:require [org.httpkit.server :as h]
+            [reader.config :as config]))
+
+(defonce server (atom nil))
+
+(defn handler
+  [request]
+  {:status 200
+   :body "mic check"})
+
+(defn start-app!
+  []
+  (reset! server (h/run-server handler {:port (config/port)}))
+  (println "Server started on port" (config/port)))
+
+(defn stop-app!
+  []
+  (when-not (nil? @server)
+    (@server :timeout 100)
+    (reset! server nil))
+  (println "Server stopped."))
+
+(defn restart-app!
+  []
+  (stop-app!)
+  (start-app!)
+  (println "Server restarted on port" (config/port)))
 
 (defn -main
   [& args]
-  (println "hello world"))
+  (start-app!))
