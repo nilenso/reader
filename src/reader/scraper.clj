fa66a90bca2baf9b4606273ef500f57fea17ffd5
Akshatha Herle <akshatha@nilenso.com>
2020-10-21 12:38:43 +0530

    Respond with html->clj->html

--- src/reader/scraper.clj
+++ src/reader/scraper.clj
@@ -1,2 +1,20 @@
-(ns reader.scraper)
+(ns reader.scraper
+  (:require [hickory.core :as h]
+            [hickory.render :as hr]))
 
+(defn format-url
+  [url])
+
+(defn get-hickory
+  [url]
+  (-> url
+      slurp
+      h/parse
+      h/as-hickory))
+
+(defn manipulate-hickory
+  [hickory])
+
+(defn get-html
+  [hickory]
+  (hr/hickory-to-html hickory))
