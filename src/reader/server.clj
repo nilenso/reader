52a451d8d4acb27c6410c37f907eb83f956ba1e7
Akshatha Herle <akshatha@nilenso.com>
2020-10-26 18:09:11 +0530

    Change back server from httpkit to ring jetty

--- src/reader/server.clj
+++ src/reader/server.clj
@@ -1,20 +1,19 @@
 (ns reader.server
-  (:require [org.httpkit.server :as s]
-            [reader.config :as c]
-            [reader.handlers :as h]))
+  (:require [reader.config :as c]
+            [reader.handlers :as h]
+            [ring.adapter.jetty :as r]))
 
 (defonce server (atom nil))
 
 (defn start-app!
   []
-  (reset! server (s/run-server h/app {:port (c/port)}))
+  (reset! server (r/run-jetty h/handler {:port (c/port), :join? false}))
   (println "Server started on port" (c/port)))
 
 (defn stop-app!
   []
-  (when-not (nil? @server)
-    (@server :timeout 100)
-    (reset! server nil))
+  (.stop @server)
+  (reset! server nil)
   (println "Server stopped."))
 
 (defn restart-app!
