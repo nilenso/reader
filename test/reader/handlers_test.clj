bd825546ca83f791dae5450f6709313792499c38
Akshatha Herle <akshatha@nilenso.com>
2020-10-27 11:44:02 +0530

    Stub http call to url in handler test

--- test/reader/handlers_test.clj
+++ test/reader/handlers_test.clj
@@ -1,11 +1,14 @@
 (ns reader.handlers_test
   (:require [clojure.test :refer :all]
-            [reader.handlers :as h]))
+            [reader.handlers :as h]
+            [reader.html :as html]))
 
 (deftest handler-test
   (testing "Given url query parameter, return status 200 response"
-    (let [response (h/fetch-page {:query-params {"url" "https://www.example.com"}})]
-      (is (= 200 (:status response)))))
+    (with-redefs [html/html-without-images (constantly "hello world")]
+      (let [request {:query-params {"url" "foo.bar"}}
+            response (h/fetch-page request)]
+        (is (= 200 (:status response))))))
   (testing "Given request without url parameter, return status 400 response"
     (let [response (h/fetch-page {})]
       (is (= 400 (:status response))))))
