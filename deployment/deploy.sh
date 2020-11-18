acb70f6cbd4a1b2cfdb5fd1cb8a3b4e624af5da3
Akshatha Herle <akshatha@nilenso.com>
2020-11-18 15:26:05 +0530

    Run app without blocking terminal

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 lsof -t -i:8080 | xargs kill -9 #kill any process running on port 8080
-java -jar reader-0.1.0-SNAPSHOT-standalone.jar
+java -jar reader-0.1.0-SNAPSHOT-standalone.jar &
