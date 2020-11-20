14dd5a5821ad981e2213939864010259c4638f28
Akshatha Herle <akshatha@nilenso.com>
2020-11-20 12:57:52 +0530

    Run uberjar as a systemd service Configure nginx	as a reverse proxy Restart reader.service through deploy script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
-lsof -t -i:8080 | xargs kill -9 #kill any process running on port 8080
-java -jar reader-0.1.0-SNAPSHOT-standalone.jar &
+systemctl status reader.service
+sudo systemctl restart reader,service
+systemctl status reader.service
